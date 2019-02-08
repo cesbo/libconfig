4885970a28b04ba3ea7de65e8c38e146f5951d1a
Andrey Dyldin <and@cesbo.com>
2019-02-08 14:26:09 +0500

    tests: move ini to file

--- tests/reader.rs
+++ tests/reader.rs
@@ -1,50 +1,28 @@
 extern crate ini;
 use ini::Section;
 
-const T1: &str = r#"
-; comment
-
-xmltv = /projects/opt/discovery.xml
-output = udp://127.0.0.1:10000
-u16 = 1234
-bool = true
-
-[multiplex]
-tsid = 1
-
-[.service]
-name = 🐽
-pnr = 1
-xmltv-id = discovery-channel
-
-[..test]
-value = test for discovery-channel
-
-[.service]
-xmltv = /projects/opt/yamal.xml
-pnr = 1185
-xmltv-id = yamal-region
-"#;
-
 #[test]
 fn test_reader() {
-    let config = Section::parse(T1.as_bytes()).unwrap();
+    let config = Section::open("tests/data/t1.ini").unwrap();
 
     assert_eq!(config.get_str("xmltv").unwrap(), "/projects/opt/discovery.xml");
     assert_eq!(config.get_str("test").unwrap_or("opt"), "opt");
     assert_eq!(config.get_bool("bool", false).unwrap(), true);
     assert_eq!(config.get_number::<u16>("u16", 0).unwrap(), 1234u16);
 
-    for multiplex in config.sections() {
-        assert_eq!(multiplex.get_name(), "multiplex");
-        assert_eq!(multiplex.get_number::<u16>("tsid", 0).unwrap(), 1);
-
-        for service in multiplex.sections() {
-            match service.get_number::<u16>("pnr", 0).unwrap() {
-                1 => assert_eq!(service.get_str("xmltv-id").unwrap(), "discovery-channel"),
-                1185 => assert_eq!(service.get_str("xmltv-id").unwrap(), "yamal-region"),
-                _ => unreachable!(),
-            }
+    for s in config.sections() {
+        match s.get_name() {
+            "multiplex" => {
+                assert_eq!(s.get_number::<u16>("tsid", 0).unwrap(), 1);
+            },
+            "service" => {
+                match s.get_number::<u16>("pnr", 0).unwrap() {
+                    1 => assert_eq!(s.get_str("xmltv-id").unwrap(), "discovery-channel"),
+                    1185 => assert_eq!(s.get_str("xmltv-id").unwrap(), "yamal-region"),
+                    _ => unreachable!(),
+                }
+            },
+            _ => unreachable!(),
         }
     }
 }
--- tests/writer.rs
+++ tests/writer.rs
@@ -1,26 +1,7 @@
+use std::fs;
+
 extern crate ini;
 use ini::*;
-
-const T1: &str = r#"xmltv = /projects/opt/discovery.xml
-output = udp://127.0.0.1:10000
-u16 = 1234
-bool = true
-
-[multiplex]
-tsid = 1
-
-[.service]
-name = 🐽
-pnr = 1
-xmltv-id = discovery-channel
-
-[..test]
-value = test for discovery-channel
-
-[.service]
-pnr = 1185
-xmltv-id = yamal-region
-"#;
 
 #[test]
 fn test_writer() {
@@ -30,28 +11,26 @@ fn test_writer() {
     config.push(Property::new("u16", 1234));
     config.push(Property::new("bool", true));
 
-    let mut m = Section::new("multiplex");
-    m.push(Property::new("tsid", 1));
+    let mut s = Section::new("multiplex");
+    s.push(Property::new("tsid", 1));
+    config.push(s);
 
-    let mut t = Section::new("test");
-    t.push(Property::new("value", "test for discovery-channel"));
     let mut s = Section::new("service");
     s.push(Property::new("name", "🐽"));
     s.push(Property::new("pnr", 1));
     s.push(Property::new("xmltv-id", "discovery-channel"));
-    s.push(t);
-    m.push(s);
+    config.push(s);
 
     let mut s = Section::new("service");
+    s.push(Property::new("xmltv", "/projects/opt/yamal.xml"));
     s.push(Property::new("pnr", 1185));
     s.push(Property::new("xmltv-id", "yamal-region"));
-    m.push(s);
-
-    config.push(m);
+    config.push(s);
 
     let mut s = Vec::<u8>::new();
     config.dump(&mut s).unwrap();
     let s = unsafe { String::from_utf8_unchecked(s) };
 
-    assert_eq!(s.as_str(), T1);
+    let t1 = fs::read_to_string("tests/data/t1.ini").unwrap();
+    assert_eq!(s.as_str(), t1.as_str());
 }
