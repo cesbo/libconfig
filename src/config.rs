7c093d359ec82da7704be90dd901903b90814e8e
Andrey Dyldin <and@cesbo.com>
2019-03-29 01:48:49 +0200

    cleaning

--- src/config.rs
+++ src/config.rs
@@ -184,7 +184,7 @@ impl Config {
                 for _ in 1 .. level {
                     last = match last.nested.last_mut() {
                         Some(v) => v,
-                        None => return Err(Error::Syntax(line, "wrong section level".into())),
+                        None => return Err(Error::Syntax(line, "wrong section level".to_owned())),
                     };
                 }
                 last.nested.push(section);
@@ -195,7 +195,7 @@ impl Config {
 
             let skip = match token.find('=') {
                 Some(v) => v,
-                None => return Err(Error::Syntax(line, "missing ‘=’ in property declaration".into())),
+                None => return Err(Error::Syntax(line, "missing ‘=’ in property declaration".to_owned())),
             };
 
             last.properties.push(Property {
